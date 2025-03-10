7c895dff1c02139935df87b2443e6e3aaa7154d8
Otaku <Otaku@PC>
2025-03-09 21:21:30 -0600

    Agregar archivos de proyecto.

---
+++ PracticaCodefirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticaCodefirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PracticaDatafirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticaDatafirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
